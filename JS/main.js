413c405eab6932191f0e64e4eb0300ed880a1581
DVMaja <majadreilinger@gmail.com>
2023-05-13 15:24:32 +0200

    nem json-nel megjelenik, javításra vár

--- JS/main.js
+++ JS/main.js
@@ -1,15 +1,15 @@
 import Termekek from "./Classes/Termekek.js";
-//import { adatLISTA } from "./Adatok/adat.js";
+import { adatLISTA } from "./Adatok/adat.js";
 import Aszinkron from "./Classes/Aszinkron.js";
 
 $(function () {
-    //new Termekek(adatLISTA);
-    const aszinkron = new Aszinkron();
-    aszinkron.adatbe("adatok.json", beolvas);
+    new Termekek(adatLISTA);
+    /* const aszinkron = new Aszinkron();
+    aszinkron.adatbe("adatok.json", beolvas); */
 })
-
+/* 
 function beolvas(adat) {
     console.log(adat);
     console.log(adat.adatLISTA);
     new Termekek(adat.adatLISTA);
-}
+} */
